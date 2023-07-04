f69754aaeec3052c453dc1966ec95a9ef4ed3f4e
Brenzxx <muhamadikbal9891@.com>
2023-07-04 20:53:20 +0700

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        bg : '#F8EDE3',
-        primary : '#7D6E83',
-        color1 : '#DFD3C3',
-        color2 : '#D0B8A8',
-        color3 : '#F2E7D5',
-        color4 : '#0E5E6F',
-        color5 : '#87805E',
-        color6 : '#FCF8E8',
-        dark : '#251B37'
-        
+        bg: "#F8EDE3",
+        primary: "#7D6E83",
+        color1: "#DFD3C3",
+        color2: "#D0B8A8",
+        color3: "#F2E7D5",
+        color4: "#0E5E6F",
+        color5: "#87805E",
+        color6: "#FCF8E8",
+        dark: "#251B37",
+      },
 
-      }
+      screens: {
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
-
+};
