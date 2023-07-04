284d9206fe4d3eb07a7daec9ad5fe840476a887d
Brenzxx <muhamadikbal9891@.com>
2023-07-04 16:07:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        bg : '#F8EDE3',
+        primary : '#7D6E83',
+        color1 : '#DFD3C3',
+        color2 : '#D0B8A8',
+        color3 : '#F2E7D5',
+        color4 : '#0E5E6F',
+        color5 : '#87805E',
+        color6 : '#FCF8E8',
+        dark : '#251B37'
+        
+
+      }
+    },
+  },
+  plugins: [],
+}
+
